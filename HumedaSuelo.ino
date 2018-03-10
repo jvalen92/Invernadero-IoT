c626b8adc659d56c4f2a3e2a7fd6bc3fd9f1338e
jvalen92 <jvalen92@eafit.edu.co>
2018-03-10 16:43:45 -0500

    valor del sensor con unidades

--- HumedaSuelo.ino
+++ HumedaSuelo.ino
@@ -1,48 +1,29 @@
-
-const int sensorPin = A0;
- 
-void setup() {
-   Serial.begin(9600);
-}
- 
-void loop() 
-{
-   int humedad = analogRead(sensorPin);
-   Serial.print(humedad);
+/*
+  # Example code for the moisture sensor
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
   
-   if(humedad < 500)
-   {
-      Serial.println("Encendido");  
-      //hacer las acciones necesarias
-   }
-   delay(1000);
-}
-
-//
-
-/* 
-Si estamos empleando la señal digital, empleamos una entrada digital para leer el estado.
- En el ejemplo mostramos un mensaje por la pantalla, 
- pero igualmente en un caso real ejecutaríamos las acciones oportunas.
- 
-const int sensorPin = 10;
- 
-void setup()
-{
-   Serial.begin(9600);
-   pinMode(sensorPin, INPUT);
-}
- 
-void loop()
-{
-   int humedad = digitalRead(sensorPin);
- 
-   //mandar mensaje a puerto serie en función del valor leido
-   if (humedad == HIGH)
-   {
-      Serial.println("Encendido");   
-      //aquí se ejecutarían las acciones
-   }
-   delay(1000);
-}
+  # the sensor value description
+  # 0  ~300     dry soil
+  # 300~700     humid soil
+  # 700~950     in water
 */
+
+void setup(){
+  
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  
+  double val=(analogRead(A0)*100)/950; //porcentaje
+
+
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(analogRead(A0));  
+  delay(100);
+  
+}
