5f9a81d014a8838e972f761ad95c9a163f3642e6
camilo0o0o <camilogiraldo31@gmail.com>
2018-11-17 18:42:05 -0500

    agregue gitignore y hice unas cosas en serialRaspberry

--- conexion_thingworx/Planta1.py
+++ conexion_thingworx/Planta1.py
@@ -11,11 +11,12 @@ if sos == 'posix':
     clear = 'clear'
 elif sos == 'nt':
     clear = 'cls'
-    
+
 # Variables
-dic={"s_luz_blanca_sise":5,"s_ph_sise":6,"s_luz_infrarroja_sise":7,"s_humedad_suelo_sise":8,"s_luz_uv_sise":9, "s_temperatura_suelo_sise":34, "s_valvula_sise":True}
-tiempo = 5 # Tiempo para enviar y recibir datos
-dics={}
+dic = {"s_luz_blanca_sise": 5, "s_ph_sise": 6, "s_luz_infrarroja_sise": 7, "s_humedad_suelo_sise": 8,
+       "s_luz_uv_sise": 9, "s_temperatura_suelo_sise": 34, "s_valvula_sise": True}
+tiempo = 5  # Tiempo para enviar y recibir datos
+dics = {}
 
 # Declaracion de estructuras utiles
 
@@ -24,46 +25,46 @@ headers = {
     'Accept': "application/json",
     'Content-Type': "application/json",
     'Cache-Control': "no-cache"
-    }
+}
 url = "http://iot.dis.eafit.edu.co/Thingworx/Things"
 
 # Valores que se obtienen
-objetos = {"planta1":{}}
+objetos = {"planta1": {}}
 
 
 # Valores oficiales
 
 claves_enviar = {
-    "Plant_sise": ["s_luz_uv_sise", "s_luz_infrarroja_sise", "s_luz_blanca_sise", "s_humedad_suelo_sise", "s_ph_sise", "s_temperatura_suelo_sise","s_valvula_sise"],
+    "Plant_sise": ["s_luz_uv_sise", "s_luz_infrarroja_sise", "s_luz_blanca_sise", "s_humedad_suelo_sise", "s_ph_sise", "s_temperatura_suelo_sise", "s_valvula_sise"],
 }
 
 claves_recibir = {
-    "Plant_sise": ["a_humedad_suelo_sise",  "a_ph_sise", "a_luz_infrarroja_sise", "a_luz_uv_sise", "a_luz_blanca_sise", "a_temperatura_suelo_sise","a_valvula_sise"],
+    "Plant_sise": ["a_humedad_suelo_sise",  "a_ph_sise", "a_luz_infrarroja_sise", "a_luz_uv_sise", "a_luz_blanca_sise", "a_temperatura_suelo_sise", "a_valvula_sise"],
 }
 
 # Valores que se enviaran
 valores_enviar = {
-    "planta1" : {
-        "s_luz_uv_sise" : "0",
-        "s_luz_infrarroja_sise" : "0",
-        "s_luz_blanca_sise" : "0",
-        "s_humedad_suelo_sise" : "0",
-        "s_ph_sise" : "0",
-        "s_temperatura_suelo_sise" : "0",
-        "s_valvula_sise" : "0" }
-   
+    "planta1": {
+        "s_luz_uv_sise": "0",
+        "s_luz_infrarroja_sise": "0",
+        "s_luz_blanca_sise": "0",
+        "s_humedad_suelo_sise": "0",
+        "s_ph_sise": "0",
+        "s_temperatura_suelo_sise": "0",
+        "s_valvula_sise": "0"}
+
 }
 
 valores_recibir = {
-    "planta1" : {
-        "a_humedad_suelo_sise" : "0",
+    "planta1": {
+        "a_humedad_suelo_sise": "0",
         "a_ph_sise": "0",
-        "a_luz_infrarroja_sise" : "0",
+        "a_luz_infrarroja_sise": "0",
         "a_luz_uv_sise": "0",
-        "a_luz_blanca_sise" : "0",
-        "a_temperatura_suelo_sise" : "0",
-        "a_valvula_sise" : "0"}
-    
+        "a_luz_blanca_sise": "0",
+        "a_temperatura_suelo_sise": "0",
+        "a_valvula_sise": "0"}
+
 }
 
 
@@ -74,40 +75,48 @@ valores_recibir = {
 def setAllServerData(valores_enviados):
     for valor in valores_enviados:
         thingTemplate = objetos[valor]["thingTemplate"]
-        setServerData(claves_enviar[thingTemplate], valores_enviados[valor], valor)
+        setServerData(claves_enviar[thingTemplate],
+                      valores_enviados[valor], valor)
     return valores_enviados
-    
+
 # Envia datos a un determinado objeto. Usamos claves y valores porque de esta forma podriamos enviar datos por separado y no
 # necesariamente enviar todos los datos de una vez
+
+
 def setServerData(claves, valores, objeto):
     for k in claves:
         tempurl = url + "/"+objeto+"_sise_v1/Properties/"+k
         payload = "{\n\t\""+k+"\":\""+valores[k]+"\"\n}"
-        response = requests.request("PUT", tempurl, data=payload, headers=headers)
+        response = requests.request(
+            "PUT", tempurl, data=payload, headers=headers)
 
 # Impresor solo valido para este contexto, ya que debe hacer .items a los contenores que recibe
+
+
 def printer(container, objet):
     containers = container.items()
-    lista=[]
+    lista = []
     for key, value in containers:
         if objet:
-            if (key != 'name' and key != 'tags' and key != 'description' and key != 'thingTemplate' ):
-                lista.append((str(key),value))
+            if (key != 'name' and key != 'tags' and key != 'description' and key != 'thingTemplate'):
+                lista.append((str(key), value))
         else:
-            lista.append((str(key),value))
-    
-    lista.sort()
-    
-    for each in lista:
-        print str(each[0]),':',each[1]
+            lista.append((str(key), value))
 
-            
+    lista.sort()
+
+    for each in lista:
+        print(str(each[0]), ':', each[1])
+
+
 # Este metodo imprime los elementos de un diccionario; en este codigo son los datos de un objeto.
 # p.e:  printData(valores["planta1"].items())
 def printData(container):
     printer(container, True)
 
 # Este metodo imprime los datos de todos los objetos
+
+
 def printAllData():
     print("Mostrando datos de todos los objetos...\n")
     for objeto in objetos:
@@ -116,45 +125,52 @@ def printAllData():
         print
 
 # Metodo que recibe los valores en el diccionario valores_recibidos
+
+
 def getAllServerData(valores_recibidos):
     for valor in valores_recibidos:
 
         valores_recibidos[valor] = getServerData(valor)
     return valores_recibidos
-           
+
 # Obtiene todas las variables y datos de un determinado objeto en thingworx y los retorna en un diccionario.
 # Devuelve una estructura que aun no conocemos muy bien, pero se debe utilizar el metodo items() para leeral
+
+
 def getServerData(objeto):
     tempurl = url + "/"+objeto+"_sise_v1/Services/GetPropertyValues"
-    
+
     response = requests.request("POST", tempurl, headers=headers)
 
     result = json.loads(response.text)['rows'][0]
-   
-    
+
     for one in result:
-        if(one[0]=='a'):
-            dics[str(one)]=result[one]
-    
+        if(one[0] == 'a'):
+            dics[str(one)] = result[one]
+
     return result
 
 
 def setProperty(key, value):
     valores_enviar["planta1"][key] = value
 
+
 def getProperty(key):
     return valores_recibir["planta1"][key]
 
+
 miObjeto = "planta1"
 
-#valores enviar s
-#valores recibir a
+# valores enviar s
+# valores recibir a
+
+
 def setValues():
     for valoruni in valores_enviar["planta1"]:
-            if(valoruni[0]=='s'):
-                        
-                setProperty(valoruni, str(dic[valoruni]))   
-  
+        if(valoruni[0] == 's'):
+
+            setProperty(valoruni, str(dic[valoruni]))
+
 
 def main():
     getAllServerData(objetos)
@@ -165,15 +181,14 @@ def main():
     # Enviar al servidor todos los datos
     setAllServerData(valores_enviar)
     printAllData()
-  
+
+
 anterior = 0
 
 
-
-
 while(True):
-        
-        main()
-        time.sleep(tiempo)
+
+    main()
+    time.sleep(tiempo)
 # por el momento se envia a thingwors los valores que el mismo pone pero deberia ser del arduino
 #
--- raspduino/conexion/SerialArduinoPython/serialRaspberry.py
+++ raspduino/conexion/SerialArduinoPython/serialRaspberry.py
@@ -1,8 +1,19 @@
 import serial
 import time
 
+# Se puso este diccionario mientras se soluciona lo de sacar el diccionario de planta 1
+valores_recibir = {
+    "a_humedad_suelo_sise": "85",
+    "a_ph_sise": "7",
+    "a_luz_infrarroja_sise": "300",
+    "a_luz_uv_sise": "350",
+    "a_luz_blanca_sise": "400",
+    "a_temperatura_suelo_sise": "40",
+    "a_valvula_sise": "1"}
+
+
 # Se establece la conexion
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('COM5', 9600, timeout=1)
 time.sleep(1)
 
 # Recibe los datos de los sensores por serial en 1 string separado por comas
@@ -10,20 +21,48 @@ time.sleep(1)
 # y con su respectivo nombre
 
 
-def getSerial():
-    data = ser.readline()
+def getArduino():
+    data = ser.readline()  # Aqui lee la linea desde serial y la guarda en una variable
+    # Se le quita todas esas cosas que deja al principio y al final del string
     data = data.strip()
+    # No se porque no quito tambien esta b, entonces toco especificar que la quite (mejorar como hace este proceso)
     data = data.decode().strip('b')
+    # Se separan los valores donde estan las ',' y se guarda en una lista
     data = data.split(',')
+
+    # aca guarda los datos en un diccionario con su respectiva clave y valor
     if len(data) == 7:
         datosDic = {"s_luz_blanca_sise": data[0], "s_ph_sise": data[1], "s_luz_infrarroja_sise": data[2],
                     "s_humedad_suelo_sise": data[3], "s_luz_uv_sise": data[4], "s_temperatura_suelo_sise": data[5],
                     "s_valvula_sise": data[6]}
-        return datosDic
+        # De prueba
+        # return datosDic
+        print(datosDic)
+    # En algunos casos las primeras veces que es llamada la funcion getSerial() como que no coge todos los valores,
+    # entonces para que no cree la lista si no llegaron los valores completos, aunque deberia pensar en una mejor
+    # forma de hacer esa parte de verificacion (le estoy pensando)
     elif len(data) < 6:
         print("no se ha llenado la lista")
 
 
+def sendArduino(valores_recibir):
+    # Por el momento voy a asumir que ya tengo los valores en un diccionario, mientras resuelvo eso de como sacarlo
+    # desde Planta1.py
+    aList = list(valores_recibir.values())
+    for value in aList:
+        ser.write(value.encode())
+        print(value)
+    print("Se acabo el proceso de enviar")
+
+
+sendArduino(valores_recibir)
+time.sleep(5)
+
+data1 = ser.readline()
 while True:
-    getSerial()
-    time.sleep(3)
+    print(data1)
+
+# para hacer pruebas
+""" while True:
+    getArduino()
+    time.sleep(2) """
